0444d2f376a5a87fe66d3b2ec31dceac599a0dc4
MagadiM <milliemagadi4@gmail.com>
2024-11-04 22:07:55 +0300

    a user input file

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name?");
+age = input("And how old are you?");
+location = input("What is your current location?")
+
+message = "Hello " + name + ". I believe you are " + age + " years old. You are from the " + location + " region and are most welcome in our territory. Feel at home!";
+
+print(message);
