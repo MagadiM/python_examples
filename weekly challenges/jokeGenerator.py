231eedb0587b358413faf21574da6f5472d6670d
MagadiM <milliemagadi4@gmail.com>
2024-11-11 23:52:26 +0300

    adding weekly challenge files

---
+++ weekly challenges/jokeGenerator.py
@@ -0,0 +1,5 @@
+import random
+
+jokes = ['Why did the two Java methods get a divorce? Because they had constant arguments', 'Why did the edge server go bankrupt? Because it ran out of cache', 'How many developers does it take to screw in a lightbulb? None, It’s a hardware problem', 'Why did the private classes break up? Because they never saw each other.'];
+
+print(random.choice(jokes));
---
+++ weekly challenges/personalizedGreeting.py
@@ -0,0 +1,4 @@
+username = input("What is your name?");
+fav_color = input("What is your favorite color?");
+message =  "Hello, " + username + "! Your favorite color " + fav_color + " is awesome."
+print(message);
